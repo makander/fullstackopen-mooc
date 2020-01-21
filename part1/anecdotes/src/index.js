18447be2863f645551d4c0839cda43a96948d642
Nils <nils@mw-008.local>
2020-01-21 13:27:37 +0100

    finish part1

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,35 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const App = props => {
   const [selected, setSelected] = useState(0);
-  const [point, setPoint] = useState(null);
+  const [vote, setVote] = useState(null)
 
-
-    //const arr = new Array(props.anecdotes.length).fill(0);
-    //return setPoint(arr)
+  
+  useEffect(() => {
+    if (vote === null) {
+      setVote(new Array(props.anecdotes.length).fill(0))
+    }
+  })
 
   const randomAnecdote = () => {
       setSelected(Math.floor(Math.random() * 6));
-      if (point.length) {
-          const arr = new Array(props.anecdotes.length).fill(0)
-          return setPoint(arr)
-      } 
   };
 
-    const addVote = () => {
-      
+  const addVote = () => {
+    let updateVote = [...vote]
+    updateVote[selected] += 1;
+    setVote([...updateVote])
+  }
+
+  const anecdoteWithHigestVote = () => {
+    
+    let highestVote = Math.max(...vote)
+    let i = vote.indexOf(Math.max(...vote));
+    let anecdotedote = props.anecdotes[i]
+
+    return (<div>
+      <h1>This has anecdote with most votes: </h1>
+      <p> {anecdotedote} which has: {highestVote} votes </p>
+    </div>)
   }
     
   return (
     <div>
+      <h1>Anecdotedote of the day</h1>
       {props.anecdotes[selected]}
       <br />
-          <Button text={"Vote"} />
-          {selected}
+      {vote ? 'this anecdotedote has ' + vote[selected] + ' votes' : null}
+     
+      <br /> <br />
+      <Button text={"Vote"} onClick={addVote}/>
       <Button text={"Next anecdote"} onClick={randomAnecdote} />
+      
+      {vote ? anecdoteWithHigestVote() : null}
+      
     </div>
   );
 };
@@ -44,33 +63,3 @@ const anecdotes = [
 ];
 
 ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
-
-/* const anecs = [
-    {
-      anecdote: "If it hurts, do it more often",
-      point: 0
-    },
-    {
-      anecdote: "Adding manpower to a late software project makes it later!",
-      point: 0
-    },
-    {
-      anecdote:
-        "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-      point: 0
-    },
-    {
-      anecdote:
-        "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-      point: 0
-    },
-    {
-      anecdote: "Premature optimization is the root of all evil.",
-      point: 0
-    },
-    {
-      anecdote:
-        "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-      point: 0
-    }
-  ]; */
