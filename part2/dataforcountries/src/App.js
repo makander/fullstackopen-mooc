b2c2c2e22a3a6fa6fbd75409eb14d88c59233c78
Nils <nils@mw-008.local>
2020-02-06 15:09:42 +0100

    finish dataforcountries

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
+ 
+
+  const [countries, setCountries] = useState([]);
+  const [filterCountries, setFilter] = useState([]);
+  const [country, setCountry] = useState(null)
+  const [weather, setWeather] = useState(null)
+
   useEffect(() => {
     axios.get('https://restcountries.eu/rest/v2/all').then((res) => {
       console.log(res.data);
@@ -10,9 +17,20 @@ function App() {
     });
   }, []);
 
-  const [countries, setCountries] = useState([]);
-  const [filterCountries, setFilter] = useState([]);
+  const getWeather = (capital) => {
+    axios.get(`http://api.weatherstack.com/current?access_key=5d391f360d0979ba6171bf50dfd4e6b5&query=${capital}`).then((res) => {
+      console.log(res.data)
+      if (res.data.error) {
+        console.log('error')
+      } else {
+        setWeather(res.data);
+      }
+    }
+    ).catch(err => console.log(err));
+  }
 
+
+  
   const handleSearch = (e) => {
     e.preventDefault();
     console.log(e.target.value);
@@ -22,32 +40,58 @@ function App() {
     console.log(filtred);
     //console.log(land)
     setFilter(filtred);
+    setCountry(null)
   };
+
+  const ShowCountry = (data) => {
+    
+    return (<div>
+      <h1>{data.name}</h1>
+      <p>Capital: {data.capital}</p>
+      <p>Population: {data.population}</p>
+      <p>Languages:</p>
+      <ul>
+       {data.languages.map((data) => (
+        <li key={data.nativeName}>{data.name}</li>
+        ))} 
+      </ul>
+      <img style={{ width: 200 }} src={data.flag} />
+
+    </div>)
+  }
+
+
+  const handleShowInfoClick = (item) => {
+    setCountry(item)
+    getWeather(item.capital)
+  }
+ 
+  const DisplayWeather = () => {
+    debugger;
+    return weather !== null ? (<div>
+      <p>Temperatur: {weather.current.temperature}</p>
+      <img src={weather.current.weather_icons[0]} />
+      <p>{weather.current.wind_speed} </p>
+    </div>) : ''
+  }
+  
 
   const SearchResult = () => {
     return (
       <div>
         {filterCountries.length > 10
           ? 'to many results'
-          : filterCountries.length < 10 && filterCountries.length > 2
-          ? filterCountries.map((item) => (
-              <li key={item.id}>
-                {item.name}
-                <button>show</button>
-              </li>
+          : filterCountries.length < 10 && filterCountries.length > 1
+          ? filterCountries.map((item) => (  
+            <p>{item.name}
+              <button onClick={() => handleShowInfoClick(item)} >show</button>
+              
+              </p>
             ))
           : filterCountries.length === 1
-          ? filterCountries.map((item) => (
-              <div>
-                <h1>{item.name}</h1>
-                <p>Capital: {item.capital}</p>
-                <p>Population: {item.population}</p>
-                <p>Languages:</p>
-                {item.languages.map((lang) => (
-                  <li>{lang.name}</li>
-                ))}
-                <img src={item.flag} />
-              </div>
+              ? filterCountries.map((item) => (
+            setCountry(item),
+            getWeather(item.capital)
             ))
           : ''}
       </div>
@@ -58,7 +102,9 @@ function App() {
     <div className="App">
       Find countries: <input onChange={handleSearch} />
       <SearchResult />
-    </div>
+      {country !== null ? ShowCountry(country) : ''}
+    <DisplayWeather />
+    </div> 
   );
 }
 
